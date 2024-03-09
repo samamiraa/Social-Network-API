7a0b9ab04bf05c20ce0cfde5e85334bb819483e1
Samantha Allen <samanthaamiraallen@gmail.com>
2024-03-09 14:11:36 -0400

    added get single user route

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,7 +9,24 @@ module.exports = {
             }
             res.status(200).json(users);
         } catch (error) {
+            console.error('Error getting users: ', error);
             res.status(500).json(error);
         }
-    }
+    },
+
+    async getOneUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+            .select('-__v');
+
+            if (!user) {
+                return res.status(404).json({ message: 'No user with that ID' });
+            }
+
+            res.status(200).json(user);
+        } catch (error) {
+            console.error('Error getting user: ', error);
+            res.status(500).json(error);
+        }
+    },
 };
--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const { getUsers } = require('../../controllers/userController');
+const { getUsers, getOneUser } = require('../../controllers/userController');
 
 router.route('/').get(getUsers);
+
+router.route('/:userId').get(getOneUser);
 
 module.exports = router;
